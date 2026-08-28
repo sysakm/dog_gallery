0e4954865efb96e0a610e005ebeb57ab9c7e0072
sysakm <sysak-ma@yandex.ru>
2026-08-28 12:07:02 +0300

    Add api layer

---
+++ src/api/dogApi.ts
@@ -0,0 +1,52 @@
+import * as axios from "axios";
+import type {DogBreed, TheDogApiBreed, TheDogApiError} from "../types/dog.ts";
+
+const DOG_API_BASE_URL = 'https://api.thedogapi.com/v1'
+
+const dogApi = axios.create({
+    baseURL: DOG_API_BASE_URL,
+    timeout: 5000,
+    headers: {
+        'x-api-key': import.meta.env.VIT_DOG_API_KEY ?? ''
+    }
+})
+
+function formatValue(value: string | undefined) {
+    return value?.trim() || 'No data'
+}
+
+function formatMetricRange(value: string | undefined, unit: string) {
+    return value?.trim() ? value.trim + ' ' + unit : 'No data'
+}
+
+function mapBreed(breed: TheDogApiBreed): DogBreed {
+    return {
+        id: String(breed.id),
+        name: breed.name,
+        description: formatValue(breed.bred_for),
+        origin: formatValue(breed.origin),
+        breedGroup: formatValue(breed.breed_group),
+        lifeSpan: formatValue(breed.life_span),
+        weight: formatMetricRange(breed.weight?.metric, 'kg'),
+        height: formatMetricRange(breed.height?.metric, 'cm'),
+        temperament: formatValue(breed.temperament),
+        mainImageUrl: breed.image?.url || null
+    }
+}
+
+export async function fetchBreeds(): Promise<Array<DogBreed>> {
+    try {
+        const response = await dogApi.get<Array<TheDogApiBreed>>(
+            '/breeds'
+        )
+        return response.data.map(mapBreed)
+    } catch (error) {
+        if (axios.isAxiosError<TheDogApiError>(error)) {
+            if (error.response) {
+                const eData = error.response.data
+                throw new Error(`${eData.statusCode }${eData.error}: ${eData.message}`);
+            }
+        }
+        throw new Error('TheDogApi request failed - something went wrong');
+    }
+}
---
+++ src/types/dog.ts
@@ -0,0 +1,38 @@
+export type RequestStatus = "idle" | "loading" | "error" | "success"
+
+export type DogBreed = {
+    id: string;
+    name: string;
+    description: string;
+    origin: string;
+    breedGroup: string;
+    lifeSpan: string;
+    weight: string;
+    height: string;
+    temperament: string;
+    mainImageUrl: string | null;
+}
+
+export type TheDogApiBreed = {
+    id: number;
+    name: string;
+    bred_for?: string;
+    breed_group?: string;
+    life_span?: string;
+    temperament?: string;
+    origin?: string;
+    reference_image_id?: string;
+    weight?: {
+        metric?: string;
+    };
+    height?: {
+        metric?: string;
+    };
+    image?: {url?: string};
+}
+
+export type TheDogApiError = {
+    statusCode: number;
+    error: string;
+    message: string;
+}
